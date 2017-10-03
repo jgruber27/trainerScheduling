26a81994c6f0b0a6d7a2005e81246f2306920dc3
kgrossnickle <kgrossnickle@ufl.edu>
2017-10-03 12:23:44 -0400

    fixed white page

--- modules/ex/client/config/ex.client.routes.js
+++ modules/ex/client/config/ex.client.routes.js
@@ -6,23 +6,11 @@
     .module('ex')
     .config(routeConfig);
 
-  routeConfig.$inject = ['$stateProvider'];
+  //routeConfig.$inject = ['$stateProvider'];
 
   function routeConfig($stateProvider) {
     // Ex state routing
     $stateProvider
-      .state('ex', {
-        url: '/ex',
-        templateUrl: 'modules/ex/client/views/ex.client.view.html',
-        controller: 'ExController',
-        controllerAs: 'vm'
-      })
-      .state('ex', {
-        url: '/ex',
-        templateUrl: 'modules/ex/client/views/ex.client.view.html',
-        controller: 'ExController',
-        controllerAs: 'vm'
-      })
       .state('ex', {
         url: '/ex',
         templateUrl: 'modules/ex/client/views/ex.client.view.html',
