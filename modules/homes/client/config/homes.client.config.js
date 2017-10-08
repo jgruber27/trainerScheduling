22cd2a65eaa690552ebea6fd178e81a4b5d42cda
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-10-08 15:59:54 -0400

    fixed and added all modules

--- modules/homes/client/config/homes.client.config.js
+++ modules/homes/client/config/homes.client.config.js
@@ -5,7 +5,7 @@
     .module('homes')
     .run(menuConfig);
 
-  menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
 
   function menuConfig(menuService) {
     // Set top bar menu items
