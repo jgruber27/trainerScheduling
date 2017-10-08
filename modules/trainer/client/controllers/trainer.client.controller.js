3d14577ecc078dedb8812cadd48da54f44fc26ce
Chris Brown <chris5175@ufl.edu>
2017-10-08 15:27:21 -0400

    creating new announcement funtion

--- modules/trainer/client/controllers/trainer.client.controller.js
+++ modules/trainer/client/controllers/trainer.client.controller.js
@@ -4,18 +4,29 @@
   angular
     .module('trainer')
     .controller('TrainerController', TrainerController);
-
+b
   TrainerController.$inject = ['$scope'];
 
   function TrainerController($scope) {
     var vm = this;
-
+    $scope.announcements{
+      $scope.dateAdded = "",
+      $scope.name = "",
+      $scope.announcement = ""
+    }
     // Trainer controller logic
     // ...
 
     init();
 
     function init() {
+    }
+
+
+    function createAnnouncement($scope){
+      $scope.name = name;
+      $scope.dateAdded = new Date();
+      $scope.announcement = announcement;
     }
   }
 })();
