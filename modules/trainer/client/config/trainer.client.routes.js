b8c2a66eb30453b892af94c75327006fa56a12d8
Chris Brown <chris5175@ufl.edu>
2017-10-09 22:19:06 -0400

    fixed issues from last pull/merge

--- modules/trainer/client/config/trainer.client.routes.js
+++ modules/trainer/client/config/trainer.client.routes.js
@@ -15,38 +15,55 @@
         url: '/trainer',
         templateUrl: 'modules/trainer/client/views/trainer.client.view.html',
         controller: 'TrainerController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['user', 'admin']
-        }
+        controllerAs: 'vm'
+        // data: {
+        //   roles: ['user', 'admin']
+        // }
       })
       .state('home', {
         url: '/',
         templateUrl: 'modules/trainer/client/views/home.client.view.html',
         controller: 'TrainerController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['user', 'admin']
-        }
+        controllerAs: 'vm'
+        // data: {
+        //   roles: ['user', 'admin']
+        // }
       })
-      .state('homadmin', {
+      .state('homeadmin', {
         url: '/homeadmin',
         templateUrl: 'modules/trainer/client/views/homeadmin.client.view.html',
         controller: 'TrainerController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['admin']
-        }
+        controllerAs: 'vm'
+        // data: {
+        //   roles: ['admin']
+        // }
       })
       .state('createAnnouncement', {
         url: '/createAnnouncement',
         templateUrl: 'modules/trainer/client/views/createAnnouncement.client.view.html',
         controller: 'TrainerController',
-        controllerAs: 'vm',
-        data: {
-          roles: ['admin']
-        }
+        controllerAs: 'vm'
+        // data: {
+        //   roles: ['admin']
+        // }
       });
+
+  }
+  getTrainer.$inject = ['$stateParams', 'TrainerService'];
+
+  function getTrainer($stateParams, TrainerService) {
+    return TrainerService.get({
+      TrainerId: $stateParams.TrainerId
+    }).$promise;
+  }
+
+  newTrainer.$inject = ['TrainerService'];
+
+  function newTrainer(TrainerService) {
+    return new TrainerService();
+  }
+}());
+
 /*
 'use strict';
 angular.module('trainer').config([
@@ -65,5 +82,3 @@ angular.module('trainer').config([
       controller: 'BlogController'
     });
 master*/
-  }
-]);
--- modules/trainer/client/controllers/trainer.client.controller.js
+++ modules/trainer/client/controllers/trainer.client.controller.js
@@ -9,37 +9,34 @@ angular.module('trainer').controller('TrainerController', [
     $scope.find = function() {
     };
 
-  angular
-    .module('trainer')
-    .controller('TrainerController', TrainerController);
+    angular.module('trainer').controller('TrainerController', TrainerController);
 
-  TrainerController.$inject = ['$scope'];
+    TrainerController.$inject = ['$scope'];
 
-  function TrainerController($scope) {
-    var vm = this;
-    // $scope.announcements{
-    //   $scope.dateAdded = "",
-    //   $scope.name = "",
-    //   $scope.title = "",
-    //   $scope.announcement = ""
-    // }
-    // Trainer controller logic
-    // ...
+    function TrainerController($scope) {
+      var vm = this;
+      // $scope.announcements{
+      //   $scope.dateAdded = "",
+      //   $scope.name = "",
+      //   $scope.title = "",
+      //   $scope.announcement = ""
+      // }
+      // Trainer controller logic
+      // ...
 
-    init();
+      init();
 
-    function init() {
+      function init() {
+      }
+
+
+      function createAnnouncement(title, announcement, $scope){
+        $scope.announcements.name = name;
+        $scope.announcements.dateAdded = new Date();
+        $scope.announcements.titles = title;
+        $scope.announcements.announcement = announcement;
+        console.log(announcement);
+        console.log(title);
+      }
     }
-
-
-    function createAnnouncement(title, announcement, $scope){
-      $scope.announcements.name = name;
-      $scope.announcements.dateAdded = new Date();
-      $scope.announcements.titles = title;
-      $scope.announcements.announcement = announcement;
-      console.log(announcement);
-      console.log(title);
-    }
-  }
-})();
-
+  }]);
