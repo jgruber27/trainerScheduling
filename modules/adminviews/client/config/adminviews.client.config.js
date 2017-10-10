068d131ae1339a62a080e7fde0432eca74392e0f
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-10-09 22:27:04 -0400

    xxRevert "fixed admin"
    This reverts commit 542e1e342af2f292a43fc9157f0b35e40b972151.

--- modules/adminviews/client/config/adminviews.client.config.js
+++ modules/adminviews/client/config/adminviews.client.config.js
@@ -16,19 +16,19 @@
       roles: ['*']
     });
 
-    // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'adminviews', {
-      title: 'List Adminviews',
-      state: 'adminviews.list'
-    });
-
-    // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'adminviews', {
-      title: 'Create Adminview',
-      state: 'adminviews.create',
-      roles: ['user']
-    });
-    //Add the dropdown My Admin View
+    // // Add the dropdown list item
+    // menuService.addSubMenuItem('topbar', 'adminviews', {
+    //   title: 'List Adminviews',
+    //   state: 'adminviews.list'
+    // });
+    //
+    // // Add the dropdown create item
+    // menuService.addSubMenuItem('topbar', 'adminviews', {
+    //   title: 'Create Adminview',
+    //   state: 'adminviews.create',
+    //   roles: ['user']
+    // });
+    // Add the dropdown My Admin View
     menuService.addSubMenuItem('topbar', 'adminviews', {
       title: 'My Main Admin View',
       state: 'adminviews.main_view',
