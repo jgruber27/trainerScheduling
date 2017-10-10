c3fd17ebd29fa72c483e2f1fcb7874da6e036a37
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-10-09 23:09:13 -0400

    added env.port

--- config/lib/app.js
+++ config/lib/app.js
@@ -38,7 +38,7 @@ module.exports.start = function start(callback) {
   _this.init(function (app, db, config) {
 
     // Start the app by listening on <port>
-    app.listen(config.port, function () {
+    app.listen(process.env.PORT || config.port, function () {
 
       // Logging initialization
       console.log('--');
