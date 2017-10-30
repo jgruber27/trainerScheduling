976f308a22aa71d0bd5a5242bacb194ba0de46da
lisbeth <lisbecg@gmail.com>
2017-10-30 18:52:21 -0400

    Added some testing functions to the file - they fail as well.

--- modules/requests/tests/e2e/requests.e2e.tests.js
+++ modules/requests/tests/e2e/requests.e2e.tests.js
@@ -26,20 +26,49 @@ describe('Requests E2E Tests:', function () {
       //Do not rely on async calls, its just directly interacts with the dom and inserts the keys into the input field.
       //browser.ignoreSynchronization = true;
       //browser.driver.findElement(protractor.By.id('name')).sendKeys('batman');
-
-      element(by.css('vm.request.name')).sendKeys(request.name);
-
-      //element(by.model('vm.request.name')).sendKeys(request.name);
-      //element(by.model('vm.request.class')).sendKeys(request.class);
-      //element(by.model('vm.request.shiftTime')).sendKeys(request.shiftTime);
-      //element(by.model('vm.request.reason')).sendKeys(request.reason);
+      browser.waitForAngular();
+      element(by.model('vm.request.name')).sendKeys(request.name);
+      element(by.model('vm.request.class')).sendKeys(request.class);
+      element(by.model('vm.request.shiftTime')).sendKeys(request.shiftTime);
+      element(by.model('vm.request.reason')).sendKeys(request.reason);
+      //Click the button to create the request.
       element(by.css('.btn btn-default')).click();
+      //browser.sleep(4000);
+      browser.waitForAngular();
+      //Check that it goes to the right link.
+      browser.get('http://localhost:3000/requests/' + request._id);
+      browser.waitForAngular();
       //element(by.id('class')).sendKeys(request.class);
       //element(by.id('shiftTime')).sendKeys(request.shiftTime);
       //element(by.id('reason')).sendKeys(request.reason);
       // element(by.css('#class')).sendKeys(request.class);
       // element(by.css('#shiftTime')).sendKeys(request.shiftTime);
       // element(by.css('#reason')).sendKeys(request.reason);
+    });
+
+    it('Accept request and go to calendar', function () {
+      //TODO: Fix this and pass the ID - Ask Kurt!!
+      browser.get('http://localhost:3000/requests/' + request._id);
+
+      element(by.css('[ng-click="vm.removeAndGoToCalendar()"]')).click();
+      browser.sleep(4000);
+
+      //TODO: Check for popup
+
+      browser.get('http://localhost:3000/schedules');
+      browser.sleep(4000);
+    });
+
+    it('Decline and delete request', function () {
+      browser.get('http://localhost:3000/requests/' + request._id);
+
+      element(by.css('[ng-click="vm.remove()"]')).click();
+      browser.sleep(4000);
+
+      //TODO: Check for popup
+
+      browser.get('http://localhost:3000/requests');
+      browser.sleep(4000);
     });
       
   });
