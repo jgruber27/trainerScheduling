05cbab8572d5c308e7a8857e56d36eafeab7a467
lisbeth <lisbecg@gmail.com>
2017-10-30 13:52:02 -0400

    Modified the testing file. Still having issues.

--- modules/requests/tests/e2e/requests.e2e.tests.js
+++ modules/requests/tests/e2e/requests.e2e.tests.js
@@ -23,11 +23,20 @@ describe('Requests E2E Tests:', function () {
     it('Test creating a new request', function () {
       //Creating a new request.
       browser.get('http://localhost:3000/requests/create');
+      //Do not rely on async calls, its just directly interacts with the dom and inserts the keys into the input field.
+      //browser.ignoreSynchronization = true;
+      //browser.driver.findElement(protractor.By.id('name')).sendKeys('batman');
 
-      element(by.id('name')).sendKeys(request.name); //same as element(by.css('name'));
-      element(by.id('class')).sendKeys(request.class);
-      element(by.id('shiftTime')).sendKeys(request.shiftTime);
-      element(by.id('reason')).sendKeys(request.reason);
+      element(by.css('vm.request.name')).sendKeys(request.name);
+
+      //element(by.model('vm.request.name')).sendKeys(request.name);
+      //element(by.model('vm.request.class')).sendKeys(request.class);
+      //element(by.model('vm.request.shiftTime')).sendKeys(request.shiftTime);
+      //element(by.model('vm.request.reason')).sendKeys(request.reason);
+      element(by.css('.btn btn-default')).click();
+      //element(by.id('class')).sendKeys(request.class);
+      //element(by.id('shiftTime')).sendKeys(request.shiftTime);
+      //element(by.id('reason')).sendKeys(request.reason);
       // element(by.css('#class')).sendKeys(request.class);
       // element(by.css('#shiftTime')).sendKeys(request.shiftTime);
       // element(by.css('#reason')).sendKeys(request.reason);
