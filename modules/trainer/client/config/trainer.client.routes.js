4980d26f2466864253d6177a4e380126f097d528
chris5175 <chris5175@ufl.edu>
2017-10-31 01:41:04 -0400

    Made it so that the announcements are limited and show in right order

--- modules/trainer/client/config/trainer.client.routes.js
+++ modules/trainer/client/config/trainer.client.routes.js
@@ -15,25 +15,19 @@
         url: '/',
         templateUrl: 'modules/trainer/client/views/home.client.view.html',
         controller: 'TrainerListController',
-        controllerAs: 'vm'
-        // data: {
-        //   roles: ['user', 'admin']
-        // }
+        controllerAs: 'vm',
+        data: {
+          roles: ['user', 'admin']
+        }
       })
       .state('homeadmin', {
         url: '/homeadmin',
         templateUrl: 'modules/trainer/client/views/homeadmin.client.view.html',
         controller: 'TrainerListController',
-        controllerAs: 'vm'
-        // data: {
-        //   roles: ['admin']
-        // }
-      })
-      .state('NotLoggedIn', {
-        url: '/NotLoggedIn',
-        templateUrl: 'modules/trainer/client/views/NotLoggedIn.client.view.html',
-        controller: 'TrainerController',
-        controllerAs: 'vm'
+        controllerAs: 'vm',
+        data: {
+          roles: ['admin']
+        }
       })
       .state('createAnnouncement', {
         url: '/createAnnouncement',
@@ -42,10 +36,10 @@
         controllerAs: 'vm',
         resolve: {
           trainerResolve: newTrainer
+        },
+        data: {
+          roles: ['admin']
         }
-        // data: {
-        //   roles: ['admin']
-        // }
       });
 
   }
@@ -64,21 +58,3 @@
   }
 }());
 
-/*
-'use strict';
-angular.module('trainer').config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider) {
-    //Listings state providing
-    $stateProvider.state('trainer', {
-      url: '/trainer',
-      templateUrl: 'modules/trainer/client/views/trainer.client.view.html',
-      controller: 'TrainerController',
-      controllerAs: 'vm'
-    }).state('blog', {
-      url: '/blog',
-      templateUrl: 'modules/trainer/client/views/blog.client.view.html',
-      controller: 'BlogController'
-    });
-master*/
