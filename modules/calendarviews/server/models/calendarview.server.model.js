2408a5848b6385867d7033eec08a3b58ed1298c9
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-10-30 23:47:54 -0400

    fixed avail and adding events

--- modules/calendarviews/server/models/calendarview.server.model.js
+++ modules/calendarviews/server/models/calendarview.server.model.js
@@ -13,9 +13,28 @@ var CalendarviewSchema = new Schema({
   name: {
     type: String,
     default: '',
-    required: 'Please fill Calendarview name',
+    required: 'Please fill in class name',
     trim: true
   },
+  trainer: {
+    type: String,
+    default: '',
+    required: 'Please fill in trainer',
+    trim: true
+  },
+  start: {
+    type: Date,
+    default: Date.now,
+    required: 'Please fill in start',
+    trim: true
+  },
+  end: {
+    type: Date,
+    default: Date.now,
+    required: 'Please fill in end',
+    trim: true
+  },
+  details: String,
   created: {
     type: Date,
     default: Date.now
@@ -23,7 +42,19 @@ var CalendarviewSchema = new Schema({
   user: {
     type: Schema.ObjectId,
     ref: 'User'
+  },
+  updated_at: Date
+});
+
+/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+CalendarviewSchema.pre('save', function(next) {
+  var currentTime = new Date();
+  this.updated_at = currentTime;
+  if(!this.created)
+  {
+    this.created = currentTime;
   }
+  next();
 });
 
 mongoose.model('Calendarview', CalendarviewSchema);
