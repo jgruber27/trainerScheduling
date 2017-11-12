b723cf078223c7ae5478c7ce95f0bd3edf498c09
lisbeth <lisbecg@gmail.com>
2017-11-12 17:23:54 -0500

    Added signin and signout vars.

--- modules/requests/tests/e2e/requests.e2e.tests.js
+++ modules/requests/tests/e2e/requests.e2e.tests.js
@@ -10,6 +10,41 @@ describe('Requests E2E Tests:', function () {
     reason: 'New class',
   };
 
+  var user_admin = {
+    firstName: 'Chris',
+    lastName: 'Brown',
+    email: 'chris5175@ufl.edu',
+    username: 'chris5175',
+    password: 'Password123!',
+    roles: 'admin'
+  };
+
+  var user_staff = {
+    firstName: 'Lisbeth',
+    lastName: 'Cardoso',
+    email: 'lisbecg@gmail.com',
+    username: 'lisbecg',
+    password: 'P@ssword123',
+    roles: 'user'
+  };
+
+  var signin = function(){
+    browser.get('http://localhost:3000/authentication/signin');
+    //Enter username
+    element(by.model('credentials.username')).sendKeys(user_admin.username);
+    //Enter password
+    element(by.model('credentials.password')).sendKeys(user_admin.password);
+    // Click Submit button
+    element(by.css('button[type="submit"]')).click();
+  };
+
+  var signout = function () {
+    // Sign out user.
+    browser.get('http://localhost:3000/authentication/signout');
+    // Delete cookies.
+    browser.driver.manage().deleteAllCookies();
+   };
+
   describe('Test Requests page', function () {
     it('Should report missing credentials', function () {
       browser.get('http://localhost:3000/requests');
@@ -19,8 +54,11 @@ describe('Requests E2E Tests:', function () {
     // it('Should list requests if credentials are not missing', function () {
     //   browser.get('http://localhost:3000/requests');
     // });
-    
+
     it('Test creating a new request', function () {
+      //Make sure user is signed in.
+      signin();
+
       //Creating a new request.
       browser.get('http://localhost:3000/requests/create');
       //Do not rely on async calls, its just directly interacts with the dom and inserts the keys into the input field.
@@ -44,6 +82,9 @@ describe('Requests E2E Tests:', function () {
       // element(by.css('#class')).sendKeys(request.class);
       // element(by.css('#shiftTime')).sendKeys(request.shiftTime);
       // element(by.css('#reason')).sendKeys(request.reason);
+
+      // Make sure user is sign out
+      signout();
     });
 
     it('Accept request and go to calendar', function () {
