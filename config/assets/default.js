22b6cd63ff85c96c516cef35b178d24c0abd762b
Jim <gruber.james27@gmail.com>
2017-11-15 15:03:07 -0500

    rich-text editor added and working!

--- config/assets/default.js
+++ config/assets/default.js
@@ -21,7 +21,10 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        
+        'public/lib/tinymce/tinymce.js',
+        'public/lib/angular-ui-tinymce/src/tinymce.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
